4079d6ae4bc89fa9280b13667811f7395772615e
lugge86 <lugge86@mailbox.org>
2020-11-01 11:24:22 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "hyperion-ng-remote adapter settings": {
+        "en": "Adapter settings for hyperion-ng-remote",
+        "de": "Adaptereinstellungen für hyperion-ng-remote",
+        "ru": "Настройки адаптера для hyperion-ng-remote",
+        "pt": "Configurações do adaptador para hyperion-ng-remote",
+        "nl": "Adapterinstellingen voor hyperion-ng-remote",
+        "fr": "Paramètres d'adaptateur pour hyperion-ng-remote",
+        "it": "Impostazioni dell'adattatore per hyperion-ng-remote",
+        "es": "Ajustes del adaptador para hyperion-ng-remote",
+        "pl": "Ustawienia adaptera dla hyperion-ng-remote",
+        "zh-cn": "hyperion-ng-remote的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "option2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
